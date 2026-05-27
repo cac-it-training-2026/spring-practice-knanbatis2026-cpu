6acf272c76e097985c935df0b76226a4dba262a7
nanba <nanba@NANBA-AEROG11.cac-k.jp.local>
2026-05-27 17:36:19 +0900

    5 作業2まで完了

--- spring_practice/src/main/java/jp/co/sss/practice/p04/q01/form/BmiForm.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p04/q01/form/BmiForm.java
@@ -1,5 +1,41 @@
 package jp.co.sss.practice.p04.q01.form;
 
 public class BmiForm {
+	private String name;
+	private double height;
+	private double weight;
+	private double bmi;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getBmi() {
+		return bmi;
+	}
+
+	public void setBmi(double bmi) {
+		this.bmi = bmi;
+	}
 
 }
