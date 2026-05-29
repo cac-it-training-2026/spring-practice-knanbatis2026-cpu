1f6199c4cba9dfc992e714adcf812d7cbf3d505c
nanba <nanba@NANBA-AEROG11.cac-k.jp.local>
2026-05-29 17:34:43 +0900

    practice5-02 まで完了
    次は p43 作業3から

--- spring_practice/src/main/java/jp/co/sss/practice/p05/q02/controller/Practice0502Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/q02/controller/Practice0502Controller.java
@@ -1,6 +1,23 @@
 package jp.co.sss.practice.p05.q02.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.sss.practice.p05.repository.FruitsSeasonRepository;
+
 // Practice6-01
+@Controller
 public class Practice0502Controller {
+	@Autowired
+	FruitsSeasonRepository repository;
+
+	@RequestMapping(path = "/fruits/list/sort/season", method = RequestMethod.GET)
+	public String showAll(Model model) {
+		model.addAttribute("orderList", repository.findAllByOrderBySeasonMonthAsc());
+		return "practice05/02/fruits_list";
+	}
 
 }
--- spring_practice/src/main/java/jp/co/sss/practice/p05/repository/FruitsSeasonRepository.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/repository/FruitsSeasonRepository.java
@@ -1,9 +1,13 @@
 package jp.co.sss.practice.p05.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jp.co.sss.practice.p05.entity.FruitsSeason;
 
 public interface FruitsSeasonRepository extends JpaRepository<FruitsSeason, Integer> {
+	List<FruitsSeason> findAllByOrderBySeasonMonthAsc();
 
+	List<FruitsSeason> findAllByOrderByFruitIdAsc();
 }
